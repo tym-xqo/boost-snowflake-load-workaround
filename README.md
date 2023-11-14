8fd061155b4e433cd42853063cc8ac491043bbd3
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2023-11-14 14:16:18 -0600

    Add script to copy data from Snowflake

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # boost-snowflake-load-workaround
+
+simple python script to dump table data from Snowflake for loading into Boost database
--- thingo.py
+++ thingo.py
@@ -1,10 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import pathlib
+
+from raw import db
+
+snowflake_url = os.getenv("SNOWFLAKE_URL")
+postgres_url = os.getenv("POSTGRES_URL")
+local_stage = "/tmp/"
 
 
-def thingo():
-    pass
+def copy_table(tbl):
+    db.engine(snowflake_url)
+    db.result("remove @boost_unload_stage", returns="proxy")
+    db.result(f"copy into @boost_unload_stage/{tbl}/ from {tbl}")
+    print(f"{tbl} copied!")
+
+
+def get_data_files(tbl):
+    db.engine(snowflake_url)
+    pathlib.Path(f"{local_stage}/{tbl}").mkdir(parents=True, exist_ok=True)
+    db.result(f"get @boost_unload_stage/{tbl} file://{local_stage}/{tbl}")
+    print(f"files fetched to {local_stage}/{tbl}")
+
+
+def list_tables():
+    db.engine(snowflake_url)
+    tbls_result = db.result(
+        "select table_name from information_schema.tables "
+        "where table_schema = 'SNOWFLAKE_BOOST_LOAD' order by table_name"
+    )
+    tbls = [tbl["table_name"] for tbl in tbls_result]
+    return tbls
+
+
+def main():
+    tbls = list_tables()
+    for tbl in tbls:
+        copy_table(tbl)
+        get_data_files(tbl)
 
 
 if __name__ == "__main__":
-    thingo()
+    main()
